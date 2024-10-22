02f55f82d3bb290b78b3e2c1591d195c1aabed93
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2024-10-22 17:25:01 +0600

    added email

--- login.c
+++ login.c
@@ -9,13 +9,13 @@
 void saveUser(struct User user) {
     FILE *file = fopen(USER_FILE, "a");
     if (file != NULL) {
-        fprintf(file, "%s,%s\n", user.username, user.password);
+        fprintf(file, "%s, %s, %s\n",user.email, user.username, user.password);
         fclose(file);
     }
 }
 
 // Function to validate login
-int loginUser(char *username, char *password) {
+int loginUser(char *email, char *password) {
     FILE *file = fopen(USER_FILE, "r");
     struct User user;
 
@@ -26,8 +26,8 @@ int loginUser(char *username, char *password) {
 
     char line[100];
     while (fgets(line, sizeof(line), file)) {
-        sscanf(line, "%[^,],%s", user.username, user.password);
-        if (strcmp(user.username, username) == 0 && strcmp(user.password, password) == 0) {
+        sscanf(line, "%[^,], %[^,],%s", user.email,user.username, user.password);
+        if (strcmp(user.email, email) == 0 && strcmp(user.password, password) == 0) {
             fclose(file);
             return 1;  // Successful login
         }
@@ -39,10 +39,13 @@ int loginUser(char *username, char *password) {
 // Function to register a new user
 void registerUser() {
     struct User newUser;
+    printf("Enter email address: ");
+    scanf("%s",newUser.email);
     printf("Enter a username: ");
     scanf("%s", newUser.username);
     printf("Enter a password: ");
     scanf("%s", newUser.password);
+
 
     saveUser(newUser);
     printf("Registration successful.\n");
@@ -50,14 +53,14 @@ void registerUser() {
 
 // Function to handle user login
 void login() {
-    char username[50], password[50];
-    printf("Enter username: ");
-    scanf("%s", username);
+    char  email[20], password[50];
+    printf("Enter email: ");
+    scanf("%s", email);
     printf("Enter password: ");
     scanf("%s", password);
 
-    if (loginUser(username, password)) {
-        printf("Login successful! Welcome, %s.\n", username);
+    if (loginUser(email, password)) {
+        printf("Login successful! Welcome, %s.\n", email);
     } else {
         printf("Invalid username or password.\n");
     }
--- login.h
+++ login.h
@@ -4,6 +4,7 @@
 struct User {
     char username[50];
     char password[50];
+    char email[20];
 };
 
 void registerUser();
